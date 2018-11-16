f050325885476edd1a9e14febe9277aa14033699
cuiyuerong <1642846909@qq.com>
2018-11-16 17:51:18 +0800

    手机号码验证

--- src/main/java/com/polystone/tools/common/StringUtil.java
+++ src/main/java/com/polystone/tools/common/StringUtil.java
@@ -402,5 +402,14 @@ public class StringUtil {
         return source;
     }
 
+    public static boolean isChinaPhoneLegal(String phone){
+        String REGEX_MOBILE = "^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$";
+        return Pattern.matches(REGEX_MOBILE, phone);
+    }
+
+    public static boolean isHKPhoneLegal(String phone){
+        String REGEX_MOBILE = "^([5,6,8,9])\\d{7}$";
+        return Pattern.matches(REGEX_MOBILE, phone);
+    }
 
 }
