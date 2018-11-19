3e1a3ff69b3ba0977fb8b306b5afc2ea01036ace
cuiyuerong <1642846909@qq.com>
2018-11-19 17:38:32 +0800

    验证邮箱

--- src/main/java/com/polystone/tools/common/StringUtil.java
+++ src/main/java/com/polystone/tools/common/StringUtil.java
@@ -412,4 +412,11 @@ public class StringUtil {
         return Pattern.matches(REGEX_MOBILE, phone);
     }
 
+    public static boolean isEmailLegal(String email){
+        String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+        return Pattern.matches(REGEX_EMAIL, email);
+    }
+
+
+
 }
